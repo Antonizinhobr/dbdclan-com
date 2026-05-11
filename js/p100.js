0b5015c85d563c15bf36daccbd4b58e04e8d5367
Antonizinhobr <anthonnybalbino2017@gmail.com>
2026-05-11 09:13:19 -0300

    Atualizacoes

--- js/p100.js
+++ js/p100.js
@@ -15,7 +15,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const MAKE_WEBHOOK_URL = "https://entidadefogueira.discloud.app/api/p100-aprovado";
+const MAKE_WEBHOOK_URL = "https://bot-black.discloud.app/api/p100-aprovado";
 const BASE_GITHUB_URL = "https://raw.githubusercontent.com/Antonizinhobr/dbdclan-com/SH4DOW/assets/img/dbd";
 
 const ADMIN_UIDS = [
